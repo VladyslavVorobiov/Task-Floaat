5d29382a223d6da5215241e0ade1d7b76a5197b5
Vladyslav Vorobiov <vlad65e@gmail.com>
2020-01-13 12:23:33 +0200

    Added effects during scroll

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,12 @@
 import handlerToRotateImagesOnFirstPage from './rotateImgFirstPage.js';
 import handlerToChangeOnPageWeDo from './changeOnPageWeDo.js';
+import effectDuringScroll from "./effectDuringScroll.js";
 
 window.addEventListener("load", mainScript);
 
 function mainScript(){
     handlerToRotateImagesOnFirstPage();
     handlerToChangeOnPageWeDo();
+    effectDuringScroll();
 }
+
