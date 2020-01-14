c9c1134c8e4d9cba2c585637aae77e6aabdf1178
Vladyslav Vorobiov <vlad65e@gmail.com>
2020-01-14 16:22:55 +0200

    added simple change photos team on click prev, next

--- scripts/changeOnPageOurTeam.js
+++ scripts/changeOnPageOurTeam.js
@@ -1,9 +1,44 @@
 export default function handlerOnPageOurTeam(){
     const pageOurTeam = document.querySelector(".section-our-team");
     const footerPageOurTeam = pageOurTeam.querySelector('footer');
+    const arrayPhotosTeam = pageOurTeam.querySelectorAll(".photoTeam");
+    const buttonPrev = pageOurTeam.querySelector(".button-previous");
+    const buttonNext = pageOurTeam.querySelector(".button-next");
+
+    const sourcePhotosTeam = [
+        "./img/photo/team1.png",
+        "./img/photo/team2.png",
+        "./img/photo/team3.png",
+        "./img/photo/team4.png",
+    ];
+    const amountPhotosTeam = sourcePhotosTeam.length;
+    let previousViewPhotosTeam = sourcePhotosTeam.concat();
+
+    const arrayOfButton = [buttonPrev, buttonNext];
+
+    arrayOfButton.forEach((item) => item.addEventListener("click", listenerButtonsSlides));
 
     footerPageOurTeam.addEventListener('mouseenter', listenerMouseOverFooterPageOurTeam);
     footerPageOurTeam.addEventListener('mouseleave', listenerMouseOutFooterPageOurTeam);
+
+    function listenerButtonsSlides(event){
+        let currentViewPhotosTeam;
+        switch (event.target.className){
+            case "button-previous":
+                let photosTeamWithoutLast = previousViewPhotosTeam.slice(0, amountPhotosTeam);
+                currentViewPhotosTeam = [ previousViewPhotosTeam[amountPhotosTeam-1], ...photosTeamWithoutLast];
+                break;
+            case "button-next":
+                currentViewPhotosTeam = previousViewPhotosTeam.slice(1);
+                currentViewPhotosTeam.push(previousViewPhotosTeam[0]);
+                break;
+            default: break;
+        }
+        arrayPhotosTeam.forEach((item, index) => {
+            item.src = currentViewPhotosTeam[index];
+        });
+        previousViewPhotosTeam = currentViewPhotosTeam.concat();
+    }
 
     function listenerMouseOverFooterPageOurTeam (){
         const footerArrow = footerPageOurTeam.querySelector('.img-arrow');
